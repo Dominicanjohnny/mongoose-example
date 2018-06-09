70f97b4787f0c37876b7adeb83f55dfec520c50d
Johnatan <johnatan@johnatans-MacBook-Air-2.local>
2018-06-09 17:00:06 -0400

    working

---
+++ example.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/exampleApp');
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Ironhacker' });
+kitty.save((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('meow');
+  }
+});
